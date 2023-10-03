4f3d91c013d2fe20d474fff9ed8323122678c061
Jo Yun <joyunhsiao@gmail.com>
2023-10-03 23:19:45 +0800

    add dynamic values in template

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 
 function App() {
+  const title = "Welcome to the new blog";
+  const likes = 50;
+  // we can output string and number
+  // we can't output boolean and object
+  const link = "https://www.google.com";
+
   return (
     <div className="App">
       <div className="content">
-        <h1>All Component</h1>
+        <h1>{ title }</h1>
+        <p>Liked { likes } times</p>
+        <p>{ 10 }</p>
+        <p>{ "hello, ninjas" }</p>
+        <p>{ [1, 2, 3, 4, 5] }</p>
+        <p>{ Math.random() * 10 }</p>
+        <a href={link}>Google Site</a>
       </div>
     </div>
   );
