0d26117731eba56c73f12767346001592f76fa7f
Joyun Hsiao 蕭若昀 <joyun.hsiao@u-media.com.tw>
2023-10-04 14:21:58 +0800

    add useEffect hook

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -13,6 +13,12 @@ const Home = () => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     };
+
+    // every rendering triggers it
+    useEffect(() => {
+        console.log("use effect ran");
+        console.log(blogs);
+    });
 
     return ( 
         <div className="home">
