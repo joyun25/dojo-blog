6d96049ce06521e9a2607b84888892a7c93d8bab
Joyun Hsiao 蕭若昀 <joyun.hsiao@u-media.com.tw>
2023-10-04 14:29:02 +0800

    add useEffect dependencies

--- src/Home.js
+++ src/Home.js
@@ -9,21 +9,32 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum ...', author: 'mario', id: 3 }
     ]);
 
+    const [name, setName] = useState("mario");
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     };
 
-    // every rendering triggers it
+    // only the first rendering triggers it
+    // useEffect(() => {
+    //     console.log("use effect ran");
+    //     console.log(blogs);
+    // }, []);
+
+    // only trigger when the first rendering and the variable mario changes
     useEffect(() => {
         console.log("use effect ran");
         console.log(blogs);
-    });
+    }, [name]);
 
     return ( 
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
             <BlogList blogs={blogs.filter(blog => blog.author === "mario")} title="Mario's Blogs"/>
+            <br />
+            <button onClick={() => setName("luigi")}>change name</button>
+            <p>{ name }</p>
         </div>
     );
 }
