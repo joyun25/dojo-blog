05ce3d9835376c6ae0f2e1c28c2a5f6354517b9f
Joyun Hsiao 蕭若昀 <joyun.hsiao@u-media.com.tw>
2023-10-04 14:04:04 +0800

    reuse components

--- src/Home.js
+++ src/Home.js
@@ -5,13 +5,14 @@ const Home = () => {
 
     const [blogs, setBlogs] = useState([
         { title: 'My new website', body: 'lorem ipsum ...', author: 'mario', id: 1 },
-        { title: 'Welcome Party!', body: 'lorem ipsum ...', author: 'mario', id: 2 },
+        { title: 'Welcome Party!', body: 'lorem ipsum ...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum ...', author: 'mario', id: 3 }
     ]);
 
     return ( 
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs"/>
+            <BlogList blogs={blogs.filter(blog => blog.author === "mario")} title="Mario's Blogs"/>
         </div>
     );
 }
