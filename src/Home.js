c189152593bc60beef2766fb71aa387dede923cb
Joyun Hsiao 蕭若昀 <joyun.hsiao@u-media.com.tw>
2023-10-04 13:25:05 +0800

    add click event

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log("get it");
+    }
+
+    const handleClickAgain = (name) => {
+        console.log("hello " + name);
+    };
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={() => handleClickAgain("Zoe")}>Click me again</button>
         </div>
      );
 }
