8a12fe7e4dbd031395c87871a08b6cc4a46ebeb5
Joyun Hsiao 蕭若昀 <joyun.hsiao@u-media.com.tw>
2023-10-04 13:30:56 +0800

    add useState hook

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,14 @@
+import { useState } from 'react';
+
 const Home = () => {
 
+    // let name = "Zoe";
+    const [name, setName] = useState("Mario");
+    const [age, setAge] = useState(25);
+
     const handleClick = () => {
-        console.log("get it");
+        setName("Luigi");
+        setAge(30);
     }
 
     const handleClickAgain = (name) => {
@@ -11,6 +18,7 @@ const Home = () => {
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{ name } is { age } years old</p>
             <button onClick={handleClick}>Click me</button>
             <button onClick={() => handleClickAgain("Zoe")}>Click me again</button>
         </div>
