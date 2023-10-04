fc163b7f70633c694d00061a8b35339d0861263a
Joyun Hsiao 蕭若昀 <joyun.hsiao@u-media.com.tw>
2023-10-04 12:40:37 +0800

    add multiple components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
   const title = "Welcome to the new blog";
@@ -9,14 +11,16 @@ function App() {
 
   return (
     <div className="App">
+      <Navbar/> {/* syntax <Navbar></Navbar> is fine too*/}
       <div className="content">
-        <h1>{ title }</h1>
+        <Home/>
+        {/* <h1>{ title }</h1>
         <p>Liked { likes } times</p>
         <p>{ 10 }</p>
         <p>{ "hello, ninjas" }</p>
         <p>{ [1, 2, 3, 4, 5] }</p>
         <p>{ Math.random() * 10 }</p>
-        <a href={link}>Google Site</a>
+        <a href={link}>Google Site</a> */}
       </div>
     </div>
   );
