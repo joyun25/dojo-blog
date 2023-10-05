a1508fbf3ddb03288c13427d4de683bd793f2075
Jo Yun <joyunhsiao@gmail.com>
2023-10-05 14:35:52 +0800

    add controlled inputs

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,40 @@
+import { useState } from 'react';
+
 const Create = () => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('mario');
+
   return ( 
     <div className="create">
       <h2>Add a new Blog</h2>
+      <form>
+        <label>Blog title:</label>
+        <input
+          type="text"
+          value={ title }
+          onChange={(e) => setTitle(e.target.value)}
+          required
+        />
+        <label>Blog content:</label>
+        <textarea
+          value={ body }
+          onChange={(e) => setBody(e.target.value)}
+          required
+        ></textarea>
+        <label>Blog author:</label>
+        <select
+          value={ author }
+          onChange={(e) => setAuthor(e.target.value)}
+        >
+          <option value="mario">mario</option>
+          <option value="yoshi">yoshi</option>
+        </select>
+        <button>Add Blog</button>
+        <p>{ title }</p>
+        <p>{ body }</p>
+        <p>{ author }</p>
+      </form>
     </div>
   );
 }
